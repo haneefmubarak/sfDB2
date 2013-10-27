051ab1f393d1fed7e5100b56bad036f855c5d290
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-10-26 21:24:52 -0700

    added function to create table

--- sfDB2.c
+++ sfDB2.c
@@ -10,6 +10,27 @@
 
 #include "sfDB2.h"
 
+table *sfDB2_mkTable (uint8_t *name, uint16_t columns, uint8_t **column_name) {
+	int32_t x;
+
+	table *newTable = malloc (sizeof (table));
+	assert (newTable);
+
+	newTable->name = strdup (name);
+	assert (newTable->name);
+	newTable->columns = columns;
+	newTable->head.status = 1;
+
+	newTable->column_name = malloc (columns * (sizeof (uint8_t *)));
+	assert (newTable->column_name);
+	for (x = 0; x < columns; x++) {
+		newTable->column_name[x] = strdup (column_name[x]);
+		assert (newTable->column_name[x]);
+	}
+
+	return newTable;
+}
+
 int main (void) {
 	printf ("sizeof:\n\tstep:\t%lu\n\trow:\t%lu\n\tcoordinate:\t%lu\n\ttable:\t%lu\n",
 		sizeof (step), sizeof (row), sizeof (coordinate), sizeof (table));
