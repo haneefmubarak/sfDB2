c9d4197837b73e8bd93088b1c045b7aa85727cc5
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-10-27 17:59:38 -0700

    fixed compiling issues + added sfDB2mkStep()

--- sfDB2.c
+++ sfDB2.c
@@ -13,7 +13,7 @@
 table *sfDB2_mkTable (uint8_t *name, uint16_t columns, uint8_t **column_name) {
 	int32_t x;
 
-	table *newTable = malloc (sizeof (table));
+	table *newTable = calloc (1, sizeof (table));
 	assert (newTable);
 
 	newTable->name = (uint8_t *) strdup ((char *) name);
@@ -30,6 +30,52 @@ table *sfDB2_mkTable (uint8_t *name, uint16_t columns, uint8_t **column_name) {
 	}
 
 	return newTable;
+}
+
+step *sfDB2_mkStep (const table *curTable, const uint8_t *id, const uint8_t id_byte) {
+	int16_t x, status;
+	step *curStep;
+	coordinate *oldCoord;
+
+	//assert (id_byte < (SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES));
+	assert (curTable->head.status);
+
+	curStep = (step *) &(curTable->head);
+
+	// Traverse the tree
+	status = curStep->type[id[0]];
+	for (x = 0; status; x++) {
+		curStep = curStep->next[id[x]];
+		status = (!(x == id_byte) && (curStep->type[id[x]] == 1));
+	}
+
+	// Ensure that we have either hit an empty * or row *, respectively
+	assert ((curStep->type[id[x]] == 0) || (curStep->type[id[x]] == 2));
+
+	// If there's already soemthing here, save it for later
+	if (curStep->type[id[x]] == 2) {
+		oldCoord = curStep->next[id[x]];
+	}
+
+	step *newStep = calloc (1, sizeof (step));
+	assert (newStep);
+
+	curStep->next[id[x]] = newStep;
+
+	// If there was something here, put it back
+	if (curStep->type[id[x]] == 2) {
+		uint8_t nextID = oldCoord->id[x];
+		((step *)(curStep->next[id[x]]))->next[nextID] = oldCoord;
+	}
+
+	curStep->type[id[x]] = 1;
+
+	// Done, but if we aren't at the id_byte we want, get there
+	if (x < id_byte) {
+		newStep = sfDB2_mkStep (curTable, id, id_byte);
+	}
+
+	return newStep;
 }
 
 int main (void) {
