d90132e987529e1bc5c3022307ba81b5882020b0
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-10-28 21:50:59 -0700

    weird spaces --> tabs

--- sfDB2.h
+++ sfDB2.h
@@ -6,33 +6,33 @@ extern uint8_t invalid_sentinel;
 #define INVALID_POINTER ((void *)&invalid_sentinel)
 
 typedef struct {
-        void *next[256];
-        uint8_t type[256];
-        uint8_t status;
+	void *next[256];
+	uint8_t type[256];
+	uint8_t status;
 } step;
 
 typedef struct {
-        uint8_t **content;
-        uint64_t *length;
-        uint64_t id;
+	uint8_t **content;
+	uint64_t *length;
+	uint64_t id;
 } row;
 
 typedef struct {
-        uint8_t *match;
-        uint8_t id[SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES];
-        uint32_t matches;
-        uint8_t status;
+	uint8_t *match;
+	uint8_t id[SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES];
+	uint32_t matches;
+	uint8_t status;
 } coordinate;
 
 typedef struct {
-        uint8_t *name, **column_name;
-        step head, rowhead;
+	uint8_t *name, **column_name;
+	step head, rowhead;
 	uint64_t rowcount;
-        uint16_t columns;
+	uint16_t columns;
 } table;
 
 table	*sfDB2_mkTable	(const uint8_t *name, const uint16_t columns,
 				const uint8_t **column_name);
 step	*sfDB2_mkStep	(const table *curTable, const uint8_t *id,
 				const uint8_t id_byte);
-step	*sfDB2_navStep (const table *curTable, const uint8_t *id);
+step	*sfDB2_navStep	(const table *curTable, const uint8_t *id);
