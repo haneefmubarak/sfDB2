306f7ac0d85b71d97da52dcc064fa85715bcb58a
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-10-30 23:24:58 -0700

    comment-docs for mkTable()

--- sfDB2.c
+++ sfDB2.c
@@ -20,15 +20,18 @@ table *sfDB2_mkTable (const uint8_t *name, const uint16_t columns,
 			const uint8_t **column_name) {
 	int32_t x;
 
+	// Fill the table with zeros so we don't have to zero specific fields
 	table *newTable = calloc (1, sizeof (table));
 	assert (newTable);
 
+	// Fill in the basic info
 	newTable->name = (uint8_t *) strdup ((char *) name);
 	assert (newTable->name);
 	newTable->columns = columns;
 	newTable->head.status = 1;
 	newTable->rowcount = 0;
 
+	// malloc the columns
 	newTable->column_name = malloc (columns * (sizeof (uint8_t *)));
 	assert (newTable->column_name);
 	for (x = 0; x < columns; x++) {
