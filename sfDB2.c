haneefmubarak/sfDB2

commit 6caa133fe7edb51778e803f75d1827013efdf9bd

fixed #2 where applicable using solution from `internalAPI.md`

sfDB2.c
@@ -48,7 +48,11 @@ step *sfDB2_mkStep (const table *curTable, const uint8_t *id,
 	step *curStep;
 	coordinate *oldCoord;
 
-	assert (id_byte < (SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES));
+	if !(id_byte < (SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES) {
+		error_sentinel[1] = strdup ("error 0x01 - id_byte is larger than its maximum");
+		return (step *) error_sentinel[1];
+	}
+
 	assert (curTable->head.status);
 
 	curStep = (step *) &(curTable->head);

commit ef611d5e91e204035ad9a2b68e89f6c6655424ad

fixed syntax error introduced in 6caa133

sfDB2.c
@@ -48,7 +48,7 @@ step *sfDB2_mkStep (const table *curTable, const uint8_t *id,
 	step *curStep;
 	coordinate *oldCoord;
 
-	if !(id_byte < (SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES) {
+	if (!(id_byte < (SHA_DIGEST_LENGTH - SFDB2_SHARD_BYTES)) {
 		error_sentinel[1] = strdup ("error 0x01 - id_byte is larger than its maximum");
 		return (step *) error_sentinel[1];
 	}