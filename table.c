31883c79272908ef33552722c99df30c0a3779fa
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-11-03 19:38:36 -0800

    solved the portions of #5 pertaining to `table.c`

--- table.c
+++ table.c
@@ -12,7 +12,6 @@ table *sfDB2_mkTable (const uint8_t *name, const uint16_t columns,
 	newTable->name = (uint8_t *) strdup ((char *) name);
 	assert (newTable->name);
 	newTable->columns = columns;
-	newTable->head.status = 1;
 	newTable->rowcount = 0;
 
 	// malloc the columns
@@ -21,6 +20,17 @@ table *sfDB2_mkTable (const uint8_t *name, const uint16_t columns,
 	for (x = 0; x < columns; x++) {
 		newTable->column_name[x] = (uint8_t *) strdup ((char *) column_name[x]);
 		assert (newTable->column_name[x]);
+	}
+
+	// malloc the column indices
+	newTable->head = malloc (sizeof (step *) * (columns + 1)); // +1 is for the row index
+	// calloc the row index
+	newTable->head[0] = calloc (1, sizeof (step));
+	newTable->head[0]->status = 1;
+	// calloc the rest
+	for (x = 1; x < (columns + 1); x++) {
+		newTable->head[x] = calloc (1, sizeof (step));
+		newTable->head[x]->status = 1;
 	}
 
 	return newTable;
